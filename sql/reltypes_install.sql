e3b3491620928afeebacb044043e9d17fe30a63f
Tim Otten <totten@civicrm.org>
2013-10-09 23:26:02 +0100

    CRM-13411 - Load relationship-types

---
+++ sql/reltypes_install.sql
@@ -0,0 +1,7 @@
+INSERT INTO `civicrm_relationship_type` 
+(`name_a_b`, `label_a_b`, `name_b_a`, `label_b_a`, `description`, `contact_type_a`, `contact_type_b`, `contact_sub_type_a`, `contact_sub_type_b`, `is_reserved`, `is_active`)
+VALUES 
+('Case Manager is','Case Manager is','Case Manager','Case Manager','Case Manager','Individual','Individual',NULL,NULL,0,1),
+('Support Staff is','Support Staff is','Support Staff','Support Staff','Support Staff','Individual','Individual',NULL,NULL,0,1),
+('Non-District Staff is','Non-District Staff is','Non-District Staff','Non-District Staff','Non-District Staff','Individual','Individual',NULL,NULL,0,1)
+;
